431da9f3c528c3aa454dcef985b93bc577217e9a
ian <ian@81319099-d35b-4c25-9717-0a4b1823a002>
2013-05-03 20:43:10 +0000

    initial conditions, error criterion
    git-svn-id: svn://nas:4000/NBody3D@1047 81319099-d35b-4c25-9717-0a4b1823a002

--- nbody3d.js
+++ nbody3d.js
@@ -42,14 +42,14 @@ function init() {
 	GLOBALS.particles[0] = { colour: GLOBALS.GREEN, Qx: 1.0, Qy: 2.0, Qz: 0.0, Px: 0.1, Py: 0.1, Pz: 0.0, mass: 5.0, };
 	GLOBALS.particles[1] = { colour: GLOBALS.RED, Qx: 2.0, Qy: 1.0, Qz: 0.0, Px: -0.1, Py: -0.1, Pz: 0.0, mass: 1.0, };
 */
-	GLOBALS.particles[0] = { colour: GLOBALS.YELLOW, Qx: 0.0, Qy: 0.0, Qz: 0.0, Px: 0.0, Py: 0.0, Pz: 0.1, mass: 100.0, };
-	GLOBALS.particles[1] = { colour: GLOBALS.WHITE, Qx: 0.0, Qy: 1.5, Qz: 0.4, Px: -3.4, Py: 0.0, Pz: -0.2, mass: 2.0, };
-	GLOBALS.particles[2] = { colour: GLOBALS.BLUE, Qx: -2.0, Qy: 0.0, Qz: -0.4, Px: 0.0, Py: -4.0, Pz: 0.2, mass: 3.0, };
-	GLOBALS.particles[3] = { colour: GLOBALS.GREEN, Qx: 3.0, Qy: 0.0, Qz: -0.2, Px: 0.0, Py: 4.0, Pz: -0.1, mass: 5.0, };
-	GLOBALS.particles[4] = { colour: GLOBALS.DARKGREY, Qx: 0.0, Qy: -4.0, Qz: 0.1, Px: 4.6, Py: 0.0, Pz: 0.1, mass: 4.0, };
-	GLOBALS.particles[5] = { colour: GLOBALS.RED, Qx: -4.0, Qy: 0.0, Qz: -0.1, Px: 0.0, Py: -2.8, Pz: -0.2, mass: 3.0, };
-	GLOBALS.particles[6] = { colour: GLOBALS.CYAN, Qx: 2.0, Qy: 0.0, Qz: -0.3, Px: 0.0, Py: 4.4, Pz: 0.2, mass: 3.0, };
-	GLOBALS.particles[7] = { colour: GLOBALS.PURPLE, Qx: 0.0, Qy: 3.0, Qz: -0.2, Px: -5.0, Py: 0.0, Pz: -0.1, mass: 4.0, };
+	GLOBALS.particles[0] = { colour: GLOBALS.YELLOW, Qx: 0.0, Qy: 0.0, Qz: 0.0, Px: 0.0, Py: 0.0, Pz: 0.0, mass: 100.0, };
+	GLOBALS.particles[1] = { colour: GLOBALS.WHITE, Qx: 0.0, Qy: 4.5, Qz: 0.4, Px: -0.2, Py: 0.0, Pz: 1.8, mass: 2.0, };
+	GLOBALS.particles[2] = { colour: GLOBALS.BLUE, Qx: -6.0, Qy: 0.0, Qz: -0.4, Px: 0.0, Py: -0.6, Pz: 1.0, mass: 3.0, };
+	GLOBALS.particles[3] = { colour: GLOBALS.GREEN, Qx: 3.0, Qy: 0.0, Qz: -0.2, Px: 0.0, Py: 5.8, Pz: -0.2, mass: 5.0, };
+	GLOBALS.particles[4] = { colour: GLOBALS.DARKGREY, Qx: 0.0, Qy: -4.0, Qz: 0.1, Px: -3.6, Py: 0.0, Pz: 0.2, mass: 4.0, };
+	GLOBALS.particles[5] = { colour: GLOBALS.RED, Qx: -4.0, Qy: 0.0, Qz: -0.1, Px: 0.0, Py: -0.2, Pz: -2.6, mass: 3.0, };
+	GLOBALS.particles[6] = { colour: GLOBALS.CYAN, Qx: 8.0, Qy: 0.0, Qz: -0.3, Px: 0.0, Py: 1.2, Pz: -0.2, mass: 3.0, };
+	GLOBALS.particles[7] = { colour: GLOBALS.PURPLE, Qx: 0.0, Qy: 4.0, Qz: -0.2, Px: -4.8, Py: 0.0, Pz: -0.2, mass: 4.0, };
 	initialize();
 	for (i = 0; i < GLOBALS.np; i += 1) {
 		a = GLOBALS.particles[i];
@@ -142,7 +142,8 @@ function render() {
 					", Min: " + GLOBALS.Hmin.toExponential(9) +
 					", Max: " + GLOBALS.Hmax.toExponential(9) +
 					", Error: " + GLOBALS.error.toExponential(9) +
-					", SNR: " + (10.0 * Math.log(Math.abs(GLOBALS.H0 / GLOBALS.error)) / Math.log(10.0)).toFixed(1));
+					", ER: " + (10.0 * Math.log(Math.abs((Hcurrent - GLOBALS.H0) / GLOBALS.H0)) / Math.log(10.0)).toFixed(1));
+//					", SNR: " + (10.0 * Math.log(Math.abs(GLOBALS.H0 / GLOBALS.error)) / Math.log(10.0)).toFixed(1));
 		}
 	}
 	GLOBALS.n += 1;
