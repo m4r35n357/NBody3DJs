aedd4aa53281e483ff731f54b39af7ede93ca6fc
ian <ian@81319099-d35b-4c25-9717-0a4b1823a002>
2013-05-18 15:18:31 +0000

    add SV8
    git-svn-id: svn://nas:4000/NBody3D@1071 81319099-d35b-4c25-9717-0a4b1823a002

--- physics.js
+++ physics.js
@@ -143,3 +143,21 @@ function stormerVerlet6 (first, second) {
 	stormerVerletBase(first, second, 0.784513610477560e0)
 }
 
+function stormerVerlet8 (first, second) {
+	stormerVerletBase(first, second, 0.104242620869991e1)
+	stormerVerletBase(first, second, 0.182020630970714e1)
+	stormerVerletBase(first, second, 0.157739928123617e0)
+	stormerVerletBase(first, second, 0.244002732616735e1)
+	stormerVerletBase(first, second, -0.716989419708120e-2)
+	stormerVerletBase(first, second, -0.244699182370524e1)
+	stormerVerletBase(first, second, -0.161582374150097e1)
+	stormerVerletBase(first, second, -0.17808286265894516e1)
+	stormerVerletBase(first, second, -0.161582374150097e1)
+	stormerVerletBase(first, second, -0.244699182370524e1)
+	stormerVerletBase(first, second, -0.716989419708120e-2)
+	stormerVerletBase(first, second, 0.244002732616735e1)
+	stormerVerletBase(first, second, 0.157739928123617e0)
+	stormerVerletBase(first, second, 0.182020630970714e1)
+	stormerVerletBase(first, second, 0.104242620869991e1)
+}
+
